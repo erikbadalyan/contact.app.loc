30a872e4af16b784d113c6a7fd281dd01a2816b4
Erik Badalyan <er.badalyan@gmail.com>
2021-10-20 15:16:51 +0400

    Lesson 1 working with Database

---
+++ lessons/commands.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * 1.   mysql -uroot -p -e "CREATE DATABASE contact_loc_db"
+ * 2.   php artisan migrate:install
+ * 3.   php artisan migrate
+ * 4.   php artisan make:migration create_companies_table
+ * 5.   php artisan migrate
+ * 6.   php artisan migrate:rollback
+ * 7.   php artisan migrate:rollback*/
