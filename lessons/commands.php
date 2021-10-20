0a1ae9d52b1e876db17b2fb945e7c4ccea086811
Erik Badalyan <er.badalyan@gmail.com>
2021-10-20 16:00:29 +0400

    Lesson 1 Eloquent

--- lessons/commands.php
+++ lessons/commands.php
@@ -10,4 +10,28 @@
  * 7.   php artisan migrate:rollback
  * 8.   php artisan make:seeder CompaniesTableSeeder
  * 9.   php artisan db:seed --class=CompaniesTableSeeder
- * 10.  php artisan db:seed*/
+ * 10.  php artisan db:seed
+ * 11.  php artisan make:model Company
+ * 12.  php artisan tinker
+ *      use app\Models\Company
+ *      Company::all()
+ *      Company::take(3)->get()
+ *      Company::first()
+ *      Company::find(1)
+ *      Company::find([1, 2, 3])
+ *      Company::where('website', 'hettinger.com')->first()
+ *
+ *      $company = new Company()
+ *      $company->name = "My company"
+ *      $company->address = "My Address"
+ *      $company->email = "mycompany@test.com"
+ *      $company->website = "mywebsite.com"
+ *      $company->save()
+ *
+ *      $company->website = "mywebsitecompany.com"
+ *      $company->save()
+ *
+ *      $company = Company::find(1)
+ *      $company->delete()
+ *      Company::destroy(11)
+ *      Company::destroy([7, 8, 9])*/
