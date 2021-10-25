eda592bbd3ea7ad5d27f2dccdf8ce8fe6d14fcad
Erik Badalyan <er.badalyan@gmail.com>
2021-10-25 16:06:57 +0400

    Exercise 2 solution complete

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -27,8 +27,7 @@ class ContactFactory extends Factory
             'last_name' => $this->faker->lastName(),
             'phone' => $this->faker->phoneNumber(),
             'email' => $this->faker->email(),
-            'address' => $this->faker->address(),
-            'company_id' => Company::pluck('id')->random()
+            'address' => $this->faker->address()
         ];
     }
 }
