da118994a51dc9eca24cab3f97a477ee9af08487
Erik Badalyan <er.badalyan@gmail.com>
2021-11-05 00:15:42 +0400

    Exercise 4 solution

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -22,9 +22,10 @@ class ContactController extends Controller
 
     public function create()
     {
+        $contact = new Contact();
         $companies = Company::orderBy('name')->pluck('name', 'id')
                             ->prepend('All Companies', '');
-        return view('contacts.create', compact('companies'));
+        return view('contacts.create', compact('companies', 'contact'));
     }
 
     public function store(Request $request) :object
@@ -45,7 +46,39 @@ class ContactController extends Controller
 
     public function show($id)
     {
-        $contact = Contact::find($id);
+        $contact = Contact::findOrFail($id);
         return view('contacts.show', compact('contact'));
+    }
+
+    public function edit($id)
+    {
+        $contact = Contact::findOrFail($id);
+        $companies = Company::orderBy('name')->pluck('name', 'id')
+            ->prepend('All Companies', '');
+        return view('contacts.edit', compact('companies', 'contact'));
+    }
+
+    public function update($id, Request $request)
+    {
+        $request->validate([
+            'first_name'    => 'required',
+            'last_name'     => 'required',
+            'email'         => 'required|email',
+            'address'       => 'required',
+            'company_id'    => 'required|exists:companies,id'
+        ]);
+
+        $contact = Contact::findOrFail($id);
+        $contact->update($request->all());
+
+        return redirect()->route('contacts.index')->with('message', 'Contact has been updated successfully');
+
+    }
+
+    public function destroy($id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+        return redirect()->route('contacts.index')->with('message', 'Contact has been deleted successfully');
     }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,21 @@
 let selectBox = document.querySelector('#filter_company_id');
+let deleteContactBtn = document.querySelectorAll('.btn-delete');
 if (selectBox) {
     selectBox.addEventListener('change', function () {
         let companyId = this.value || this.options[this.selectedIndex].value;
         window.location.href = window.location.href.split('?')[0] + '?company_id=' + companyId;
     });
 }
+
+deleteContactBtn.forEach(function (button){
+    button.addEventListener('click', function (event){
+        event.preventDefault();
+        if (confirm('Are you sure?')) {
+            let action = this.getAttribute('href');
+            let form = document.querySelector('#form-delete')
+            form.setAttribute('action', action);
+            form.submit();
+        }
+    });
+});
 
