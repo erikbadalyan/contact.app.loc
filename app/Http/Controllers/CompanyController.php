eea754d0777de947eddf60e92bd4bebe8e9b457e
Erik Badalyan <er.badalyan@gmail.com>
2021-11-26 15:50:10 +0400

    Exercise 6 solution part 1 Create Resource Controller for Companies

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,84 +3,102 @@
 namespace App\Http\Controllers;
 
 use App\Models\Company;
-use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
+use Illuminate\View\View;
+use App\Http\Requests\CompanyRequest;
 
 class CompanyController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth', 'verified']);
+    }
     /**
      * Display a listing of the Companies.
      *
-     * @return Response
+     * @return View
      */
-    public function index(): Response
+    public function index(): View
     {
+        $companies = auth()->user()->companies()->latest()->paginate(10);
 
+        return view('companies.index', compact('companies'));
     }
 
     /**
      * Show the form for creating a new Company.
      *
-     * @return Response
+     * @return View
      */
-    public function create(): Response
+    public function create(): View
     {
+        $company = new Company;
 
+        return view('companies.create', compact('company'));
     }
 
     /**
      * Store a newly created company in companies table.
      *
-     * @param  Request  $request
-     * @return Response
+     * @param  CompanyRequest  $request
+     * @return RedirectResponse
      */
-    public function store(Request $request): Response
+    public function store(CompanyRequest $request): RedirectResponse
     {
+        $request->user()->companies()->create($request->all());
 
+        return redirect()->route('companies.index')->with('message', "Company has been added successfully");
     }
 
     /**
      * Display the specified company.
      *
      * @param  Company  $company
-     * @return Response
+     * @return View
      */
-    public function show(Company $company): Response
+    public function show(Company $company): View
     {
-
+        return view('companies.show', compact('company'));
     }
 
     /**
      * Show the form for editing the specified company.
      *
      * @param  Company  $company
-     * @return Response
+     * @return View
      */
-    public function edit(Company $company): Response
+    public function edit(Company $company): View
     {
-
+        return view('companies.show', compact('company'));
     }
 
     /**
      * Update the specified company in companies table.
      *
-     * @param  Request  $request
+     * @param  CompanyRequest  $request
      * @param  Company  $company
-     * @return Response
+     * @return RedirectResponse
      */
-    public function update(Request $request, Company $company): Response
+    public function update(CompanyRequest $request, Company $company): RedirectResponse
     {
+        $company->update($request->all());
 
+        return redirect()->route('companies.index')->with('message', "Company has been updated successfully");
     }
 
     /**
      * Remove the specified company from companies table.
      *
      * @param  Company  $company
-     * @return Response
+     * @return RedirectResponse
      */
-    public function destroy(Company $company): Response
+    public function destroy(Company $company): RedirectResponse
     {
+        $company->delete();
 
+        return redirect()
+            ->route('contacts.index')
+            ->with('message', 'Contact has been deleted successfully');
     }
 }
