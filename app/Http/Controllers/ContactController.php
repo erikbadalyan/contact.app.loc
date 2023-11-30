e251d68284af9d24b5b19dcd6f984d7de8bc014e
Erik Badalyan <er.badalyan@gmail.com>
2023-11-30 16:00:31 +0400

    CRUD and search functionality for contacts

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Contact;
 use App\Repositories\CompanyRepository;
+use Illuminate\Support\Facades\DB;
 
 class ContactController extends Controller {
     public function __construct(protected CompanyRepository $company)
@@ -14,9 +15,29 @@ class ContactController extends Controller {
     public function index()
     {
         $companies = $this->company->pluck();
+        // DB::enableQueryLog();
+        // $contacts = Contact::latest()->where(function ($query) {
+        //     if ($companyId = request()->query("company_id")) {
+        //         $query->where("company_id", $companyId);
+        //     }
+
+        //     if ($search = request()->query("search")) {
+        //         $query->where('first_name', 'LIKE', "%{$search}%");
+        //         $query->orWhere('last_name', 'LIKE', "%{$search}%");
+        //         $query->orWhere('email', 'LIKE', "%{$search}%");
+        //     }
+        // })->paginate(10);
+        // dump(DB::getQueryLog());
+
         $contacts = Contact::latest()->where(function ($query) {
             if ($companyId = request()->query("company_id")) {
                 $query->where("company_id", $companyId);
+            }
+        })->where(function ($query) {
+            if ($search = request()->query('search')) {
+                $query->where("first_name", "LIKE", "%{$search}%");
+                $query->orWhere("last_name", "LIKE", "%{$search}%");
+                $query->orWhere("email", "LIKE", "%{$search}%");
             }
         })->paginate(10);
         return view('contacts.index', compact('contacts', 'companies'));
@@ -24,12 +45,56 @@ class ContactController extends Controller {
 
     public function create()
     {
-        return view('contacts.create');
+        $companies = $this->company->pluck();
+        return view('contacts.create', compact('companies'));
     }
 
     public function show($id)
     {
         $contact = Contact::findOrFail($id);
         return view('contacts.show')->with('contact', $contact);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|string|max:50',
+            'last_name' => 'required|string|max:50',
+            'email' => 'required|email',
+            'phone' => 'nullable',
+            'address' => 'nullable',
+            'company_id' => 'required|exists:companies,id',
+        ]);
+        Contact::create($request->all());
+        return redirect()->route('contacts.index')->with('message', 'Contact has been added successfully');
+    }
+
+    public function edit($id)
+    {
+        $companies = $this->company->pluck();
+        $contact = Contact::findOrFail($id);
+        return view('contacts.edit', compact('companies', 'contact'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $contact = Contact::findOrFail($id);
+        $request->validate([
+            'first_name' => 'required|string|max:50',
+            'last_name' => 'required|string|max:50',
+            'email' => 'required|email',
+            'phone' => 'nullable',
+            'address' => 'nullable',
+            'company_id' => 'required|exists:companies,id'
+        ]);
+        $contact->update($request->all());
+        return redirect()->route('contacts.index')->with('message', 'Contact has been updated successfully');
+    }
+
+    public function destroy($id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+        return redirect()->route('contacts.index')->with('message', 'Contact has been removed successfully');
     }
 }
